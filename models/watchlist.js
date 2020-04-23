7423828e9e1db19ffc391731356b63845ce56ea1
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-23 10:56:06 -0700

    Made a model

--- models/watchlist.js
+++ models/watchlist.js
@@ -1,19 +1,17 @@
-module.exports = function(sequelize,Datatypes){
-    const watchListItem = sequelize.define("WatchListItem",{
-        api_id: {
-            type: Datatypes.STRING,
-            allowNull: false
-        },
-        isWatched: {
-            type: Datatypes.BOOLEAN,
-            allowNull:false,
-            default: false
-        },
-        foreignKey: {
-            allowNull:false,
-
-        }
-
-    })
-    return watchListItem
-}
+module.exports = function(sequelize, Datatypes) {
+  const watchListItem = sequelize.define("WatchListItem", {
+    api_id: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    isWatched: {
+      type: Datatypes.BOOLEAN,
+      allowNull: false,
+      default: false
+    },
+    foreignKey: {
+      allowNull: false
+    }
+  });
+  return watchListItem;
+};
