fb2c32d4bcd599a0de8db2b50f60f466e4c8474e
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-26 16:02:37 -0700

    All pages are styled

--- public/js/search.js
+++ public/js/search.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     }).then(function(response) {
       // Creating a div to hold the movie
       // Retrieving the URL for the image
-      const searchResult = `<div class="row">
+      const searchResult = `<div class="row" id ="searched-movie">
       <div class="col xl6 s12">
       <h2 id="movie-title">${response.Title}</h2>
       <br>
