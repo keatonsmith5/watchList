b12078c7bb332a71adcbed2fe6050c9f33f42e54
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-26 21:26:17 -0700

    Converted everything to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup", { style: "stylesheets/style.css" });
   });
 
   app.get("/login", function(req, res) {
@@ -18,13 +18,13 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login", { style: "stylesheets/style.css" });
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("members", { style: "stylesheets/members.css" });
   });
 
   //route to handle switching from search movies to my movielist.
